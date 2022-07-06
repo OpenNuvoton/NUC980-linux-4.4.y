c3c257563ab9663636eb284faa8b62ab40ef4a63
MS10 CWWeng <cwweng@nuvoton.com>
2022-07-06 17:17:34 +0800

    Add Giga Device GD25B256E SPI Flash support
    Change-Id: I745650ab8e4aecc7c1770085743031df8fbd2ce0

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -701,6 +701,7 @@ static const struct flash_info spi_nor_ids[] = {
 	{ "gd25q32", INFO(0xc84016, 0, 64 * 1024,  64, SECT_4K) },
 	{ "gd25q64", INFO(0xc84017, 0, 64 * 1024, 128, SECT_4K) },
 	{ "gd25q128", INFO(0xc84018, 0, 64 * 1024, 256, SECT_4K) },
+	{ "gd25b256e", INFO(0xc84019, 0, 64 * 1024, 256, SECT_4K) },
 
 	/* Intel/Numonyx -- xxxs33b */
 	{ "160s33b",  INFO(0x898911, 0, 64 * 1024,  32, 0) },
